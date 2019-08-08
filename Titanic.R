dae5481340935e6cf082472067851c9e9fbe7808
YuSeongHun <devjupiter97@gmail.com>
2019-08-08 20:25:40 +0900

    dd

--- Titanic.R
+++ Titanic.R
@@ -10,17 +10,26 @@ test  <- read.csv('./dataset/test.csv', stringsAsFactors = F)
 full  <- bind_rows(train, test)
 str(full)
 
+# Grab title from passenger names
 full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
+
+# Show title counts by sex
 table(full$Sex, full$Title)
 
+# Titles with very low cell counts to be combined to "rare" level
 rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
                 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
+
+# Also reassign mlle, ms, and mme accordingly
 full$Title[full$Title == 'Mlle']        <- 'Miss' 
 full$Title[full$Title == 'Ms']          <- 'Miss'
 full$Title[full$Title == 'Mme']         <- 'Mrs' 
 full$Title[full$Title %in% rare_title]  <- 'Rare Title'
+
+# Show title counts by sex again
 table(full$Sex, full$Title)
 
+# Finally, grab surname from passenger name
 full$Surname <- sapply(full$Name,  
                        function(x) strsplit(x, split = '[,.]')[[1]][1])
 cat(paste('We have <b>', nlevels(factor(full$Surname)), 
